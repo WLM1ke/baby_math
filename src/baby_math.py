cc16ec1fb4b524e1ddd8bc9c5c31a4a6c3d8e654
WLM1ke <wlmike@gmail.com>
2018-06-14 19:40:04 +0300

    Класс примера

---
+++ src/baby_math.py
@@ -0,0 +1,8 @@
+"""Основной файл - выдает примеры и проверяет ответы"""
+
+from src.data_managment import load_data
+
+if __name__ == '__main__':
+    cases = load_data()
+    heap = []
+
--- src/data_managment.py
+++ src/data_managment.py
@@ -6,18 +6,45 @@ from pathlib import Path
 from src.cases_generators import cases_all
 
 DATA_PATH = Path(__file__).parent / 'data' / 'results.json'
+INDENT = 0
+
+
+class Case:
+    def __init__(self, case, last_time):
+        self._case = case
+        self._last_time = last_time
+
+    def __repr__(self):
+        return f'\nCase(case={self._case}, last_time={self._last_time})'
+
+    @property
+    def case(self):
+        return self._case
+
+    @property
+    def last_time(self):
+        return self._last_time
+
+    def check(self, result):
+        return eval(self._case) == eval(result)
 
 
 def load_data():
     if DATA_PATH.exists():
         with open(DATA_PATH, mode='r') as file:
-            return json.load(file)
+            raw_cases = json.load(file)
     else:
         DATA_PATH.parent.mkdir(parents=True)
-        cases = [[case, None] for case in cases_all()]
+        raw_cases = [[case, None] for case in cases_all()]
         with open(DATA_PATH, mode='w') as file:
-            json.dump(cases, file, indent=4)
-        return cases
+            json.dump(raw_cases, file, indent=INDENT)
+    return [Case(case, last_time) for case, last_time in raw_cases]
+
+
+def save_data(cases):
+    with open(DATA_PATH, mode='w') as file:
+        raw_cases = [[case.case, case.last_time] for case in cases]
+        json.dump(raw_cases, file, indent=INDENT)
 
 
 if __name__ == '__main__':
